43577cd79c19189905d647c36e7765cfb7e47217
Christopher Deehan <Deehan-C7@ulster.ac.uk>
2024-10-08 10:57:17 +0100

    24T Completed

--- Library/Book.cs
+++ Library/Book.cs
@@ -8,17 +8,43 @@ namespace Library
 {
     class Book
     {
-        string Title;
-        string Author;
-        string ISBN;
-        int Pages;
+        private string title;
+        private string author;
+        private string isbn;
+        private int pages;
+
+        // Title property to allow access
+        // to the title private variable
+        public string Title
+        {
+            get { return title; } // get method
+            set { title = value; } // set method
+        }
+
+        public string Author
+        {
+            get { return author; } // get method
+            set { author = value; } // set method
+        }
+
+        public string ISBN
+        {
+            get { return isbn; } // get method
+            set { isbn = value; } // set method
+        }
+
+        public int Pages
+        {
+            get { return pages; } // get method
+            set { pages = value; } // set method
+        }
 
         public Book(string bookTitle, string bookAuthor, string bookISBN, int bookPages)
         {
-            Title = bookTitle;
-            Author = bookAuthor;
-            ISBN = bookISBN;
-            Pages = bookPages;
+            this.title = bookTitle;
+            this.author = bookAuthor;
+            this.isbn = bookISBN;
+            this.pages = bookPages;
         }
 
         public void DisplayInfo()
