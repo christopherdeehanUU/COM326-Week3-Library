2efacd013bdc26b6fde0ba1815745f6982731904
Christopher Deehan <Deehan-C7@ulster.ac.uk>
2024-10-08 10:49:15 +0100

    23T Completed.

---
+++ Library/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Book
+    {
+        string Title;
+        string Author;
+        string ISBN;
+        int Pages;
+
+        public Book(string bookTitle, string bookAuthor, string bookISBN, int bookPages)
+        {
+            Title = bookTitle;
+            Author = bookAuthor;
+            ISBN = bookISBN;
+            Pages = bookPages;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Book Information");
+            Console.WriteLine("----------------");
+            Console.WriteLine($"Book title: {Title}");
+            Console.WriteLine($"Book Author: {Author}");
+            Console.WriteLine($"Book ISBN: {ISBN}");
+            Console.WriteLine($"Number of pages: {Pages}");
+            Console.WriteLine();
+        }
+    }
+}
--- Library/Program.cs
+++ Library/Program.cs
@@ -1,28 +1,6 @@
-﻿class Book
+﻿using Library;
+class Program
 {
-    string Title;
-    string Author;
-    string ISBN;
-    int Pages;
-
-    public Book(string bookTitle, string bookAuthor, string bookISBN, int bookPages)
-    {
-        Title = bookTitle;
-        Author = bookAuthor;
-        ISBN = bookISBN;
-        Pages = bookPages;
-    }
-
-    void DisplayInfo()
-    {
-        Console.WriteLine("Book Information");
-        Console.WriteLine("----------------");
-        Console.WriteLine($"Book title: {Title}");
-        Console.WriteLine($"Book Author: {Author}");
-        Console.WriteLine($"Book ISBN: {ISBN}");
-        Console.WriteLine($"Number of pages: {Pages}");
-        Console.WriteLine();
-    }
 
     static void Main(string[] args)
     {
